ed894646641dbf57164347e059caba7d3f57bc89
otumian <popecan1000@gmail.com>
2022-06-29 03:48:33 +0000

    todo app with django and django-rest-framework

---
+++ todoapp/todos/views.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from rest_framework import viewsets, permissions
+
+from .models import Todo
+from .serializer import TodoSerializer
+
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = Todo.objects.all().order_by("id")
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
